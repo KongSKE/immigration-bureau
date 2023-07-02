4145cab3241268da94519b48d2163ec32492a4e4
varito <varit.as@ku.th>
2023-07-03 01:48:18 +0700

    add helmet()

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
+    .addBearerAuth()
     .setTitle('The Immigration Bureau')
     .setDescription('The Immigration Bureau API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.use(helmet());
+  app.enableCors({
+    origin: [/localhost(:\d+)?$/],
+  });
   await app.listen(3000);
 }
 bootstrap();
